9e24108b7d11b97cb3d04b8972233a4ea6e47d80
rltvty2 <32552121+rltvty2@users.noreply.github.com>
2026-03-15 19:51:08 +0100

    Update ulli-o-windows-run.bat

--- ulli-o-windows-run.bat
+++ ulli-o-windows-run.bat
@@ -1,4 +1,4 @@
 
-powershell -Command "Start-Process -Verb RunAs powershell '-NoExit -ExecutionPolicy Bypass -Command "%userprofile%\Downloads\ulli-organic\ulli-o-windows.ps1"'"
+powershell -Command "Start-Process -Verb RunAs powershell '-NoExit -ExecutionPolicy Bypass -Command "%userprofile%\Downloads\ulli-organic-main\ulli-o-windows.ps1"'"
 
 pause
