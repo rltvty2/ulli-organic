7c2ce37fc9bdf943272d184f61c06904d9cddf61
rltvty2 <32552121+rltvty2@users.noreply.github.com>
2026-03-18 09:02:32 +0100

    Add files via upload

--- ulli-o-windows.ps1
+++ ulli-o-windows.ps1
@@ -1,3 +1,28 @@
+
+echo "ULLI-organic USB-less Linux Installer"
+
+echo "Options:"
+
+echo "1: Install Linux Mint 22.3"
+
+echo "2: Install other ISO"
+
+Do {
+
+$option = Read-Host -Prompt 'Type 1 or 2 and hit Enter to select option'
+
+}
+
+Until ($option -eq '1' -or $option -eq '2' )
+
+Do {
+
+$file = Read-Host -Prompt 'Full path and name of ISO file (including .iso)'
+
+}
+
+Until ( [System.IO.File]::Exists($file) -eq "True" )
+
 $PartInfo = Get-Partition -DriveLetter C
 $DiskInfo = Get-Disk $PartInfo.DiskNumber
 $ShrinkTo = $DiskInfo.Size - (7 * 1024 * 1024 * 1024)
@@ -9,11 +34,25 @@ Format-Volume -DriveLetter L -FileSystem FAT32
 
 Import-Module BitsTransfer
 
-Start-BitsTransfer -Source "https://pub.linuxmint.io/stable/22.3/linuxmint-22.3-cinnamon-64bit.iso" -Destination "C:\linuxmint.iso"
+if ( $option -eq 1 )
+{
+	
+    Start-BitsTransfer -Source "https://pub.linuxmint.io/stable/22.3/linuxmint-22.3-cinnamon-64bit.iso" -Destination "C:\linuxmint.iso"
+	
+	$iso_location = "C:\linuxmint.iso"
+	
+}
+
+if ( $option -eq 2 )
+{
+	
+	$iso_location = $file
+	
+}
 
 Start-BitsTransfer -Source "https://sourceforge.net/projects/refind/files/0.14.2/refind-bin-0.14.2.zip/download" -Destination "C:\refind.zip"
 
-$mountResult = Mount-DiskImage -ImagePath "C:\linuxmint.iso" -PassThru
+$mountResult = Mount-DiskImage -ImagePath $iso_location -PassThru
 
 $driveLetter = ($mountResult | Get-Volume).DriveLetter + ":\"
 
