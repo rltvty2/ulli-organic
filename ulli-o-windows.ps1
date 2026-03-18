3c8c3ccc3be149a14ff74c0adb4e6003ecd3cf31
rltvty2 <32552121+rltvty2@users.noreply.github.com>
2026-03-18 21:11:48 +0100

    Add files via upload

--- ulli-o-windows.ps1
+++ ulli-o-windows.ps1
@@ -41,16 +41,36 @@ if ( $option -eq 2 )
 	
 }
 
+echo "Would you like to install rEFInd boot manager as well? PLEASE BE AWARE THAT YOU MUST DISABLE SECURE BOOT IN THE BIOS TO BOOT USING THIS OPTION"
+
+Do {
+
+	$option2 = Read-Host -Prompt "Type y or n for yes or no, then hit enter"
+
+}
+
+Until ($option2 -eq 'y' -or $option2 -eq 'n' )
+
+if ( $option2 -eq 'y' )
+{
+	
+    Start-BitsTransfer -Source "https://sourceforge.net/projects/refind/files/0.14.2/refind-bin-0.14.2.zip/download" -Destination "C:\refind.zip"
+	
+}
+
 $PartInfo = Get-Partition -DriveLetter C
 $DiskInfo = Get-Disk $PartInfo.DiskNumber
 $ShrinkTo = $DiskInfo.Size - (7 * 1024 * 1024 * 1024)
 Resize-Partition -DriveLetter C -Size $ShrinkTo
-New-Partition -DiskNumber 0 -Size 100MB -DriveLetter V
-New-Partition -DiskNumber 0 -UseMaximumSize -DriveLetter L
-Format-Volume -DriveLetter V -FileSystem FAT32
-Format-Volume -DriveLetter L -FileSystem FAT32
 
-Start-BitsTransfer -Source "https://sourceforge.net/projects/refind/files/0.14.2/refind-bin-0.14.2.zip/download" -Destination "C:\refind.zip"
+if ( $option2 -eq 'y' )
+{
+	New-Partition -DiskNumber 0 -Size 100MB -DriveLetter V
+	Format-Volume -DriveLetter V -FileSystem FAT32
+}
+
+New-Partition -DiskNumber 0 -UseMaximumSize -DriveLetter L
+Format-Volume -DriveLetter L -FileSystem FAT32
 
 $mountResult = Mount-DiskImage -ImagePath $iso_location -PassThru
 
@@ -58,7 +78,10 @@ $driveLetter = ($mountResult | Get-Volume).DriveLetter + ":\"
 
 robocopy $driveLetter "L:\" /E /ZB
 
+if ( $option2 -eq 'y' )
+{
 Expand-Archive C:\refind.zip -DestinationPath V:\
+}
 
 bcdboot C:\Windows
 
@@ -70,11 +93,16 @@ bcdedit /set "{bootmgr}" path \EFI\boot\bootx64.efi
 
 bcdedit /copy "{bootmgr}" /d "Linux"
 
-bcdedit /set "{bootmgr}" device partition=V:
+if ( $option2 -eq 'y' )
+{
 
-bcdedit /set "{bootmgr}" path refind-bin-0.14.2\refind\refind_x64.efi
+	bcdedit /set "{bootmgr}" device partition=V:
 
-bcdedit /set "{bootmgr}" description "rEFInd"
+	bcdedit /set "{bootmgr}" path refind-bin-0.14.2\refind\refind_x64.efi
+
+	bcdedit /set "{bootmgr}" description "rEFInd"
+
+}
 
 bcdedit /set "{fwbootmgr}" default "{bootmgr}"
 
